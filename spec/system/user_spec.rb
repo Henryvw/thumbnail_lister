c88445518397978f3bc0dfd0aa2d2854ab2f5051
Henry van Wagenberg <henryvw1@gmail.com>
2019-10-26 14:59:30 +0200

    Create user setup

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  let!(:normal_user) { FactoryBot.create(:user) }
-
   it 'can register a new account' do
+    visit root_path
+    click_link 'Register'
+    fill_in 'user_email', with: 'thumb_lover123@gmail.com'
+    fill_in 'user_password', with: '1234fingers'
+    fill_in 'user_password_confirmation', with: '1234fingers'
+    click_button 'Regiser'
+    expect(page).to have_content 'Welcome to Thumbnail Lister!'
+    expect(page).to have_http_status(:success)
   end
 
   it 'can log in with its account infos' do
