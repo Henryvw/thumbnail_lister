7dc3f9f299a3ad8e82fa8abe538c41505157471a
Henry van Wagenberg <henryvw1@gmail.com>
2019-10-28 18:53:18 +0100

    Add test for thumbnail index

--- spec/system/thumbnail_spec.rb
+++ spec/system/thumbnail_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe 'Thumbnail', type: :system do
     attach_file('thumbnail_picture', 'spec/support/assets/test-image.JPG')
     click_button 'Upload It'
   end
+
+  it 'can view a List of Thumbnails when logged in' do
+    existing_user = FactoryBot.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'session_email', with: existing_user.email
+    fill_in 'session_password', with: existing_user.password
+    click_button 'Log In'
+    click_link 'Add a Thumbnail here'
+    attach_file('thumbnail_picture', 'spec/support/assets/test-image.JPG')
+    click_button 'Upload It'
+    click_link 'List my Thumbnails'
+  end
 end
